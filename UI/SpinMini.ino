a4f34c4e6f502496e0765bf2b105bf800c670da8
Lwin Paing Oo <78170253+Lwin-Oo@users.noreply.github.com>
2023-12-08 10:09:15 -0600

    Update SpinMini.ino

--- UI/SpinMini.ino
+++ UI/SpinMini.ino
@@ -6,23 +6,34 @@
 #define ROTARY_PIN2     6
 #define BUTTON_PIN      7
 
-#define CLICKS_PER_STEP 4   // this number depends on your rotary encoder
+#define CLICKS_PER_STEP 4 
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Updated for a 20x4 display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 Rotary r;
 Button2 b;
 
-int menuIndex = 0; // Current menu item index
+const char circle[] = {
+  B00100,
+  B01010,
+  B10001,
+  B10001,
+  B01010,
+  B00100
+};
 
-// Specify the custom order of menu items
+int menuIndex = 0;
+int rotationAngle = 0;
+
 const char *customOrder[] = {"SPIN", "STOP", "QUIT"};
 
 void setup() {
   lcd.init();
   lcd.backlight();
-  Serial.println("\n\nWELCOME");
-  delay(50);
+  displayWelcomeAnimation(); // Display a fancy welcome message
+  delay(2000); // Display for 2 seconds
+  lcd.clear();
+
   r.begin(ROTARY_PIN1, ROTARY_PIN2, CLICKS_PER_STEP);
   r.setChangedHandler(rotate);
   r.setLeftRotationHandler(rotateRight);
@@ -30,10 +41,10 @@ void setup() {
 
   b.begin(BUTTON_PIN);
   b.setTapHandler(click);
-  b.setLongClickHandler(resetPosition);
+  //b.setLongClickHandler(resetPosition);
 
-  lcd.begin(20, 4); // Initialize the LCD with 20 columns and 4 rows
-  lcd.print(customOrder[menuIndex]); // Display the initial menu item
+  lcd.println(" Spinner Menu ");
+  lcd.print(customOrder[menuIndex]);
 }
 
 void loop() {
@@ -41,48 +52,75 @@ void loop() {
   b.loop();
 }
 
-// on change
 void rotate(Rotary &r) {
   // Handle rotation
 }
 
-// on left rotation
 void rotateLeft(Rotary &r) {
-  lcd.clear(); // Clear the LCD screen
-
-  // Increment menuIndex and wrap around if necessary
   menuIndex = (menuIndex - 1 + sizeof(customOrder) / sizeof(customOrder[0])) % (sizeof(customOrder) / sizeof(customOrder[0]));
-
-  lcd.print(customOrder[menuIndex]); // Display the new menu item
-  Serial.println("Selecting: " + String(customOrder[menuIndex]));
+  lcd.clear();
+  lcd.print(customOrder[menuIndex]);
 }
 
-// on right rotation
 void rotateRight(Rotary &r) {
-  lcd.clear(); // Clear the LCD screen
-
-  // Increment menuIndex and wrap around if necessary
   menuIndex = (menuIndex + 1) % (sizeof(customOrder) / sizeof(customOrder[0]));
-
-  lcd.print(customOrder[menuIndex]); // Display the new menu item
-  //Serial.println("Selecting: " + String(customOrder[menuIndex]));
+  lcd.clear();
+  lcd.print(customOrder[menuIndex]);
 }
 
-// single click
 void click(Button2 &btn) {
-  lcd.clear(); // Clear the LCD screen
-  lcd.print("Selected: " + String(customOrder[menuIndex])); // Display the selected menu item
-  //Serial.println("Selected: " + String(customOrder[menuIndex]));
+  lcd.clear();
+  if (String(customOrder[menuIndex]) == "SPIN") {
+    
+    lcd.print("Motor Spinning");
+    
+
+  } else if (String(customOrder[menuIndex]) == "STOP") {
+    lcd.print("Motor Stopped");
+  } else if (String(customOrder[menuIndex]) == "QUIT") {
+    displayGoodbyeAnimation();
+    delay(2000); // Display the animation for 2 seconds
+    lcd.noBacklight(); // Turn off the LCD backlight
+    delay(1000); // Keep the LCD turned off for 1 second
+    lcd.backlight(); // Turn on the LCD backlight
+    lcd.clear();
+    displayWelcomeAnimation();
+    delay(2000); // Display "Welcome" for 2 seconds
+    //lcd.clear();
+    //menuIndex = 0;
+  }
 }
 
-// long click
-void resetPosition(Button2 &btn) {
-  lcd.clear(); // Clear the LCD screen
-  lcd.print("Reset Position");
-  delay(1000);
-  lcd.clear(); // Clear the LCD screen
-  lcd.print(customOrder[menuIndex]); // Display the current menu item
-  //Serial.println("Reset!");
+//void resetPosition(Button2 &btn) {
+  //lcd.clear();
+  //lcd.print("Reset Position");
+  //delay(1000);
+  //lcd.clear();
+  //lcd.print(customOrder[menuIndex]);
+//}
+
+void displayWelcomeAnimation() {
+  lcd.clear();
+
+  // Display a simple animation loop
+  for (int i = 0; i < 10; ++i) {
+    lcd.setCursor(i, 1);
+    lcd.print("CoSE");
+    delay(200);
+    lcd.clear();
+  }
+}
+
+void displayGoodbyeAnimation() {
+  lcd.clear();
+
+  // Display a goodbye animation loop
+  for (int i = 0; i < 3; ++i) {
+    lcd.setCursor(i, 1);
+    lcd.print("+");
+    delay(500);
+    lcd.clear();
+  }
 }
 
 
